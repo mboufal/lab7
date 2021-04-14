6dbf2001eea7e48e77c69241d5d8a9282e194823
mboufal <77710124+mboufal@users.noreply.github.com>
2021-04-13 20:37:40 -0500

    script
    java script code added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
+	var but = document.getElementById('entrybutton');
+	var output = document.getElementById('textoutput');
+	var input = document.getElementById('entryinput');
+		function myEventFunction(){
+			alert('Magda Boufal: ' + input.value);
+			output.innerHTML = input.value;
+		}
+		but.addEventListener('click', init);
+}
 
 window.addEventListener('load', init);
